89bb87ea77de98c8d82dc5f593fd2076024fddd4
Orhan <ob52636@ubt-uni.net>
2022-12-16 12:05:53 +0100

    LoginForm

---
+++ scriptLogin.js
@@ -0,0 +1,102 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+ } 
+
+ if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+ } 
+
+const form = document.getElementById('form');
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    Validate();
+    SuccessMsg();
+})
+
+const sendData = (sRate, Count) => {
+    if(sRate === Count){
+        swal("Hello! ", "Welcome");
+    }
+}
+
+function SuccessMsg() {
+    let formContr = document.getElementsByClassName('form-control');
+    var Count = formContr.length - 1;
+    for(var i = 0; i < formContr.length; i++){
+        if(formContr[i].className === "form-control success"){
+            var sRate = 0 + i;
+           
+            sendData(sRate, Count);
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
+const isEmail = (emailVal) =>{
+    var atSymbol = emailVal.indexOf('@');
+    if(atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if(dot <= atSymbol + 2) return false;
+    if(dot === emailVal.length -1) return false;
+    return true;
+}
+
+//Validimi
+function Validate(){
+   
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+   
+   
+    //email
+    if(emailVal === ""){
+        setErrorMsg(email, 'email cannot be blank');
+    }
+    else if(!isEmail(emailVal)){
+        setErrorMsg(email, 'email is not valid');
+    }
+    else{
+        setSuccessMsg(email);
+    }
+
+    //password
+    if(passwordVal === ""){
+        setErrorMsg(password, 'password cannot be blank');
+    }
+    else if(passwordVal.length <= 7){
+        setErrorMsg(password, 'min 8 char');
+    }
+    else{
+        setSuccessMsg(password);
+    }
+
+    
+}
+
+function setErrorMsg(input, errormsgs){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = "form-control error";
+    small.innerText = errormsgs;
+}
+
+function setSuccessMsg(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
