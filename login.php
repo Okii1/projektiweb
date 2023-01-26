7d4a41436302073a4c50f2759cb065b72656de28
Orhan <ob52636@ubt-uni.net>
2023-01-26 21:38:45 +0100

    some changes in login.php

--- login.php
+++ login.php
@@ -6,11 +6,8 @@ session_start();
 
 if(isset($_POST['submit'])){
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $user_type = $_POST['user_type'];
 
    $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
 
@@ -35,36 +32,32 @@ if(isset($_POST['submit'])){
    }else{
       $error[] = 'incorrect email or password!';
    }
-   
-}
+
+};
 ?>
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>Sign Up Form</title>
     <link rel="stylesheet" href="style.css">
         
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
 </head>
-<body>  
+<body>
+  
+<div class="signup">
+    <div class="container">
+        <div class="header">
+            <h2 style="padding-left : 155px;color:white; font-size: 40px;">SignUp Form</h2>
+        </div>
 
-   
+    
+      <div class="form-control">
 
-
-    <div class="signup">
-        <div class="container">
-            <div class="header">
-                <h2 style="padding-left : 155px;color:white; font-size: 40px;">Login Form</h2>
-            </div>
-            <form action="" class="form" id="form">
-                
-               
-                <div class="form-control">
-                <form action="" method="post">
-      
+<form action="" method="post">
       <?php
       if(isset($error)){
          foreach($error as $error){
@@ -72,18 +65,16 @@ if(isset($_POST['submit'])){
          };
       };
       ?>
-      <label> Email</label>
+      <br>
+      <label>Email</label>
       <input type="email" name="email" required placeholder="enter your email">
       <label>Password</label>
       <input type="password" name="password" required placeholder="enter your password">
-      <label> </label>
-      <input type="submit" name="submit" value="login now" class="btnn">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>don't have an account? <a href="SignUp.php">register now</a></p>
    </form>
-   </div>
-   <p>don't have an account? <a href="SignUp.php"><button class="normal">Sign Up</button></a></p>
 
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    
-    <script src="scriptLogin.js"></script>
+</div>
+
 </body>
 </html>
