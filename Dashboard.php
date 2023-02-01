c6ca942315f09ce0cd6aee715dac2b842bdda5e6
Orhan <ob52636@ubt-uni.net>
2023-02-01 16:57:24 +0100

    lidhja e kontakt me databaz

--- Dashboard.php
+++ Dashboard.php
@@ -8,13 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-<?php
 
-@include 'config.php';
-
-
-
-?>
     
     <input type="checkbox" id="sidebar-toggle">
     <div class="sidebar">
@@ -64,107 +58,29 @@
                         <h3>Contact</h3>
                         
                         <div class="table-responsive">
-                            <table>
+                        <table>
                                 <thead>
                                     <tr>
                                         <th>Name</th>
                                         <th>Email</th>
                                         <th>Subject</th>
                                         <th>Message</th>
-                                        
                                     </tr>
-                                </thead>
-                                <tbody>
+                                    <tbody>  <?php
+                         $connect = mysqli_connect("localhost","root","","user_db");
+                         $query = "SELECT * FROM contact_form";
+                         $result = mysqli_query($connect,$query);
+                         while($row = mysqli_fetch_array($result)){?>
+                                    
                                     <tr>
-                                        <td>App Development</td>
-                                        <td>15 Aug, 2020</td>
-                                        <td>22 Aug, 2020</td>
-                                        </td>
-                                        <td>
-                                            <span class="badge success">Success</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Logo Design</td>
-                                        <td>15 Aug, 2020</td>
-                                        <td>22 Aug, 2020</td>
-                                        </td>
-                                        <td>
-                                            <span class="badge warning">Processing</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Server setup</td>
-                                        <td>15 Aug, 2020</td>
-                                        <td>22 Aug, 2020</td>
-                                        </td>
-                                        <td>
-                                            <span class="badge success">Success</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Front-end Design</td>
-                                        <td>15 Aug, 2020</td>
-                                        <td>22 Aug, 2020</td>
-                                        </td>
-                                        <td>
-                                            <span class="badge warning">Processing</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Web Development</td>
-                                        <td>15 Aug, 2020</td>
-                                        <td>22 Aug, 2020</td>
-                                        </td>
-                                        <td>
-                                            <span class="badge success">Success</span>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    
-                    <div class="summary">
-                        <div class="summary-card">
-                            <div class="summary-single">
-                                <span class="ti-id-badge"></span>
-                                <div>
-                                    <h5>196</h5>
-                                    <small>Number of staff</small>
-                                </div>
-                            </div>
-                            <div class="summary-single">
-                                <span class="ti-calendar"></span>
-                                <div>
-                                    <h5>16</h5>
-                                    <small>Number of leave</small>
-                                </div>
-                            </div>
-                            <div class="summary-single">
-                                <span class="ti-face-smile"></span>
-                                <div>
-                                    <h5>12</h5>
-                                    <small>Profile update request</small>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="bday-card">
-                            <div class="bday-flex">
-                                <div class="bday-img"></div>
-                                <div class="bday-info">
-                                    <h5>Dwayne F. Sanders</h5>
-                                    <small>Birthday Today</small>
-                                </div>
-                            </div>
-                            
-                            <div class="text-center">
-                                <button>
-                                    <span class="ti-gift"></span>
-                                    Wish him
-                                </button>
-                            </div>
+                         <td> <?=$row['name'] ?>  </td> 
+                         <td><?=$row['email'] ?>   </td>
+                         <td><?=$row['subject'] ?>    </td>
+                         <td><?=$row['message'] ?>     </td>  
+                         </tr>
+               
+           <?php }
+?>  
                         </div>
                     </div>
                 </div>
